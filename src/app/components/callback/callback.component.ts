ab5aeca51b502f08176001ce1ab814a760a534c7
vex malamov <vex_malamov@abv.bg>
2020-01-11 18:33:54 +0200

    generating recommendations

--- src/app/components/callback/callback.component.ts
+++ src/app/components/callback/callback.component.ts
@@ -10,6 +10,8 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./callback.component.scss']
 })
 export class CallbackComponent implements OnInit {
+  genres: string[] = [];
+  seedsArtists: string[] = [];
 
   constructor(
     private route: ActivatedRoute, 
@@ -26,15 +28,44 @@ export class CallbackComponent implements OnInit {
       .subscribe(res => {
         this.spotifyApiService.token = res.access_token;
 
-        this.getUserTopArtists();
+        this.getUserTopArtistsData();
       });
   }
 
-  getUserTopArtists(){
+
+  async getUserTopArtistsData(){
     this.spotifyApiService.getUserTopArtists()
       .subscribe((data) => {
-        console.log(data);
+        let allGenres: string[] = [];
+
+        data['items'].forEach(item => {
+          allGenres = allGenres.concat(item.genres);
+        });
+
+        this.seedsArtists = data['items'].map((item) => item.id).slice(0, 2);
+
+        const allGenresCount = allGenres
+          .reduce(function(prev, cur) {
+            prev[cur] = (prev[cur] || 0) + 1;
+            return prev;
+          }, {});
+
+        const topFiveGenres = Object.keys(allGenresCount)
+          .sort((a, b) => allGenresCount[b]-allGenresCount[a])
+          .slice(0, 3);
+
+        this.genres = topFiveGenres;
+
+        this.generateSeed();
       });
-  }
+    }
+
+    generateSeed(){
+      console.log(this.genres);
+      this.spotifyApiService.recommendationsBasedOnSeeds(this.genres, this.seedsArtists)
+        .subscribe((data) => {
+          console.log(data);
+        })
+    }
 
 }
--- src/app/core/spotify-api.service.ts
+++ src/app/core/spotify-api.service.ts
@@ -15,4 +15,11 @@ export class SpotifyApiService {
   getUserTopArtists(){
     return this.http.get(`${this.baseUrl}/me/top/artists?time_range=medium_term&limit=10`);
   }
+
+  recommendationsBasedOnSeeds(genres: string[], artists: string[]){
+    console.log(genres, artists)
+    return this.http.get(`${this.baseUrl}/recommendations?limit=5&` +
+      `seed_artists=${encodeURIComponent(artists.join(','))}` + 
+      `&seed_genres=${encodeURIComponent(genres.join(','))}`);
+  }
 }
